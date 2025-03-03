11c3af635dd5cf2c4ed2051d1159d9b358fc1996
nesreen <nesrafarhan@gmail.com>
2025-03-03 20:41:57 +0200

    app theme and profile ui

---
+++ lib/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xffFFFFFF);
+  static const Color black = Color(0xff121312);
+  static const Color red = Color(0xffE82626);
+  static const Color green = Color(0xff57AA53);
+  static const Color yellow = Color(0xffFFBB3B);
+  
+
+  static ThemeData LightTheme = ThemeData();
+  static ThemeData DarkTheme = ThemeData(
+      primaryColor:primary,
+   
+     textTheme: TextTheme(
+      displaySmall:TextStyle(fontSize: 36,fontWeight: FontWeight.bold,color: primary),
+      headlineSmall:TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: primary),
+      titleLarge:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: primary), 
+      titleMedium:TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: primary), 
+      titleSmall:TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: primary),
+     ), 
+
+
+
+
+
+  );
+
+ 
+
+}
