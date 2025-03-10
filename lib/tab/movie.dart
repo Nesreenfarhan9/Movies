e7149b18608feb58d4b840284d6935411d3f6654
TasneemSafwat <tasmarakby92@gmail.com>
2025-03-10 03:40:38 +0200

    implement movie class

---
+++ lib/tab/movie.dart
@@ -0,0 +1,12 @@
+class Movie {
+  final String name;
+  final String image;
+  final String rating;
+  final String genre;
+
+  Movie(
+      {required this.name,
+      required this.image,
+      required this.rating,
+      required this.genre});
+}
