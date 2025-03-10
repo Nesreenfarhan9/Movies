defbff037b7af2f46c3962e39d1054711763eaeb
nesreen <nesrafarhan@gmail.com>
2025-03-10 23:30:43 +0200

    Backup android folder before recreating

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/feature_auth/presentation/cubit/auth_cubit.dart';
+import 'package:movies/movie_bloc.dart';
+import 'package:movies/movie_event.dart';  
+import 'package:movies/movie_repository.dart';
 import 'package:movies/tab/profile_tab/update.dart';
 import 'package:movies/shared/app_theme.dart';
 import 'package:movies/tab/profile_tab/profile%20_page_screen.dart';
@@ -10,33 +13,37 @@ import 'feature_auth/presentation/screens/login.dart';
 import 'feature_auth/presentation/screens/register_screen.dart';
 import 'onboarding/widgets/home_onboarding_screen.dart';
 
-
 void main() {
-  runApp(MoviesApp());
+  final movieRepository = MovieRepository(); 
 
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+        BlocProvider(create: (_) => MovieBloc(movieRepository)..add(FetchMovies())),
+      ],
+      child: MoviesApp(),
+    ),
+  );
 }
 
 class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthCubit(),
-      child: MaterialApp(
-        routes: {
-          ProfilePageScreen.routeNamed: (_) => ProfilePageScreen(),
-          Update.routeNamed: (_) => Update(),
-
-         LoginScreen.routeNamed: (_) => LoginScreen(),
-          HomeOnboardingScreen.routeNamed: (_) => HomeOnboardingScreen(),
-          ForgetPasswordScreen.routeNamed: (_) => ForgetPasswordScreen(),
-          RegisterScreen.routeNamed: (_) => RegisterScreen(),
-        },
-        initialRoute:  HomeOnboardingScreen.routeNamed,
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.LightTheme,
-        darkTheme: AppTheme.DarkTheme,
-        themeMode: ThemeMode.dark,
-      ),
+    return MaterialApp(
+      routes: {
+        ProfilePageScreen.routeNamed: (_) => ProfilePageScreen(),
+        Update.routeNamed: (_) => Update(),
+        LoginScreen.routeNamed: (_) => LoginScreen(),
+        HomeOnboardingScreen.routeNamed: (_) => HomeOnboardingScreen(),
+        ForgetPasswordScreen.routeNamed: (_) => ForgetPasswordScreen(),
+        RegisterScreen.routeNamed: (_) => RegisterScreen(),
+      },
+      initialRoute: HomeOnboardingScreen.routeNamed,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.LightTheme,
+      darkTheme: AppTheme.DarkTheme,
+      themeMode: ThemeMode.dark,
     );
   }
 }
