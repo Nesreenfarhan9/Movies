aa241098b1be637b619fe7e42bc8a85db5f25fb4
nesreen <nesrafarhan@gmail.com>
2025-03-12 03:37:16 +0200

    implement search logic

--- lib/tab/search_screen.dart
+++ lib/tab/search_screen.dart
@@ -1,7 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:movies/shared/app_theme.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _searchController = TextEditingController();
+  List movies = [];
+
+  Future<void> searchMovies(String query) async {
+    if (query.isEmpty) return;
+    final url = Uri.parse('https://yts.mx/api/v2/list_movies.json?query_term=$query');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        movies = data['data']['movies'] ?? [];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,11 +45,11 @@ class SearchScreen extends StatelessWidget {
                       'assets/icons/search.png',
                     ),
                     color: AppTheme.primary),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Expanded(
                   child: TextField(
+                    controller: _searchController,
+                    onChanged: searchMovies,
                     decoration: InputDecoration(
                       hintText: "Search",
                       hintStyle: TextStyle(
@@ -40,16 +63,27 @@ class SearchScreen extends StatelessWidget {
               ],
             ),
           ),
-       
         ),
-          
         Expanded(
-          child: Center(
-            child: Image.asset("assets/images/image.png", width: 140, height: 150),
-          ),
+          child: movies.isEmpty
+              ? Center(
+                  child: Image.asset("assets/images/image.png",
+                      width: 140, height: 150),
+                )
+              : ListView.builder(
+                  itemCount: movies.length,
+                  itemBuilder: (context, index) {
+                    final movie = movies[index];
+                    return ListTile(
+                      leading: Image.network(movie['medium_cover_image']),
+                      title: Text(movie['title'], style: TextStyle(color: Colors.white)),
+                      subtitle: Text("Year: ${movie['year']}", style: TextStyle(color: Colors.white70)),
+                    );
+                  },
+                ),
         ),
       ],
     );
-    
   }
 }
+
