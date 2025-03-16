7dee2e72116cc251e2f9edb3435055bc91a4470b
mhmdw <mhmdwdyn65@gmail.com>
2025-03-16 17:46:15 +0200

    merge our code

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MoviesApp extends StatelessWidget {
           MoviesDetailsScreen.routeNamed: (_) => MoviesDetailsScreen(),
            resetPasswordScreen.routeNamed: (_) => resetPasswordScreen(),
         },
-        initialRoute: ProfilePageScreen.routeNamed,
+        initialRoute: LoginScreen.routeNamed,
         debugShowCheckedModeBanner: false,
         theme: AppTheme.LightTheme,
         darkTheme: AppTheme.DarkTheme,
