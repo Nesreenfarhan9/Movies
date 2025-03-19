e763c1ab9be0abc44a6a4bc2545f46d812274077
tasneem safwat <tasneemstep@gmail.com>
2025-03-19 22:55:01 +0200

    Added my latest changes

--- lib/tabs/sebha/sebha_tab.dart
+++ lib/tabs/sebha/sebha_tab.dart
@@ -5,6 +5,22 @@ class SebhaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset('assets/images/header.png'),
+        Text(
+          'سَبِّحِ اسْمَ رَبِّكَ الأعلى ',
+          style: Theme.of(context).textTheme.bodyMedium,
+          textAlign: TextAlign.center,
+        ),
+        Stack(
+          children: [
+            Image.asset('assets/images/Sebhahead'),
+            Image.asset('assets/images/SebhaBody'),
+          ],
+        )
+      ],
+    );
   }
 }
